1a4fe9a81e91fa68a5657522f413f52a7c92425e
Eyal Toledano <eutait@gmail.com>
2019-10-02 17:00:08 -0400

    Done.

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -1,4 +1,5 @@
 class FoursquareService
+
   def authenticate!(client_id, client_secret, code)
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
       req.params['client_id'] = client_id
@@ -11,4 +12,13 @@ class FoursquareService
     body = JSON.parse(resp.body)
     body["access_token"]
   end
+
+  def friends(token)
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+  
 end
